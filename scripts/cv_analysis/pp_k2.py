73437c721f8c2c9be48617418297f3f24750513a
muellerdo <domey038@live.de>
2021-05-05 17:22:39 +0200

    Started working on cross-validation analysis

---
+++ scripts/cv_analysis/pp_k2.py
@@ -0,0 +1,48 @@
+#==============================================================================#
+#  Author:       Dominik Müller                                                #
+#  Copyright:    2020 IT-Infrastructure for Translational Medical Research,    #
+#                University of Augsburg                                        #
+#                                                                              #
+#  This program is free software: you can redistribute it and/or modify        #
+#  it under the terms of the GNU General Public License as published by        #
+#  the Free Software Foundation, either version 3 of the License, or           #
+#  (at your option) any later version.                                         #
+#                                                                              #
+#  This program is distributed in the hope that it will be useful,             #
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of              #
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the               #
+#  GNU General Public License for more details.                                #
+#                                                                              #
+#  You should have received a copy of the GNU General Public License           #
+#  along with this program.  If not, see <http://www.gnu.org/licenses/>.       #
+#==============================================================================#
+#-----------------------------------------------------#
+#                   Library imports                   #
+#-----------------------------------------------------#
+import tensorflow as tf
+from miscnn.data_loading.interfaces import NIFTI_interface
+from miscnn import Data_IO
+from miscnn.evaluation.cross_validation import split_folds
+
+#-----------------------------------------------------#
+#      Tensorflow Configuration for GPU Cluster       #
+#-----------------------------------------------------#
+# physical_devices = tf.config.list_physical_devices('GPU')
+# tf.config.experimental.set_memory_growth(physical_devices[0], True)
+
+#-----------------------------------------------------#
+#                Running Preprocessing                #
+#-----------------------------------------------------#
+# Initialize Data IO Interface for NIfTI data
+## We are using 4 classes due to [background, lung_left, lung_right, covid-19]
+interface = NIFTI_interface(channels=1, classes=4)
+
+# Create Data IO object to load and write samples in the file structure
+data_io = Data_IO(interface, input_path="data", delete_batchDir=False)
+
+# Access all available samples in our file structure
+sample_list = data_io.get_indiceslist()
+sample_list.sort()
+
+# Split samples into k (training, validation) folds
+split_folds(sample_list, k_fold=2)
